1a63fc0489bdbac32cd80af7e9468d85fa3dbb40
Nitesh Jain <nitesh.jain@bcone.com>
2017-12-16 20:39:28 +0530

    Since and until feature implemented

--- scrappers/Twitter_searching.py
+++ scrappers/Twitter_searching.py
@@ -1,9 +1,10 @@
+import datetime
 import json
 
 import pandas as pd
 import tweepy
 
-settings = json.load(open('credentials_tokens.json'))
+settings = json.load(open('settings.json'))
 keywords = pd.read_csv('keywords.csv')
 profile_link_prefix = 'https://twitter.com/'
 
@@ -34,42 +35,48 @@ if not api:
     exit(-1)
 
 
-def parse_search_configuration(config):
-    if config['country'] or config['city']:
-        outh = tweepy.OAuthHandler(API_KEY, API_SECRET)
-        outh.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
-        api = tweepy.API(outh)
+def get_search_place(config):
+    outh = tweepy.OAuthHandler(API_KEY, API_SECRET)
+    outh.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+    api = tweepy.API(outh)
 
-        query = config['country'] if config['country'] else config['city']
-        granularity = 'country' if config['country'] else 'city'
-        places = api.geo_search(query=query, granularity=granularity)
+    query = config['country'] if config['country'] else config['city']
+    granularity = 'country' if config['country'] else 'city'
+    places = api.geo_search(query=query, granularity=granularity)
 
-        if len(places) <= 0:
-            print("City or Country you entered doesn't exist!")
-            exit(-1)
-        else:
-            return places[0].id
+    if len(places) <= 0:
+        print("City or Country you entered doesn't exist!")
+        exit(-1)
+    else:
+        return places[0].id
 
 
-print(parse_search_configuration(config))
+def get_search_date(date):
+    try:
+        datetime.datetime.strptime(date, '%Y-%m-%d')
+        return date
+    except ValueError:
+        print("Incorrect data format, should be YYYY-MM-DD")
 
 
 # search tweet's for keywords
-def search_twitter_for(search_keyword):
+def search_twitter_for(**kwargs):
     search_count = 0
-    print("searching for: " + search_keyword)
+    print("searching for: " + kwargs['query'])
+
     with open('search.json', 'w') as f:
-        for status in tweepy.Cursor(api.search, q=search_keyword,
+        for status in tweepy.Cursor(api.search, q=kwargs['query'],
                                     count=tweet_per_query,
-                                    since_id=None).items():
+                                    since=kwargs['since'], until=kwargs['until']).items():
             tweet_json = status._json
             f.writelines(json.dumps(tweet_json, indent=4, sort_keys=True))
-            lead_names.append(tweet_json['user']['name'])
-            lead_handles.append(tweet_json['user']['screen_name'])
-            lead_bio.append(tweet_json['user']['description'])
-            lead_verified.append(tweet_json['user']['verified'])
+            user = tweet_json['user']
+            lead_names.append(user['name'])
+            lead_handles.append(user['screen_name'])
+            lead_bio.append(user['description'])
+            lead_verified.append(user['verified'])
             search_count += 1
-            print(tweet_json['user']['name'])
+            print(tweet_json['created_at'])
         search_leads['Name'] = pd.Series(lead_names)
         search_leads['Handle'] = pd.Series(lead_handles)
         search_leads['Verified'] = pd.Series(lead_verified)
@@ -78,10 +85,25 @@ def search_twitter_for(search_keyword):
         print('Found {0} matching tweets.'.format(search_count))
 
 
-# for key in keywords['Keywords'].append(keywords['HashTags']):
-search_twitter_for(keywords['Keywords'][2])
-#
-search_leads.drop_duplicates(subset=['Handle'], keep=False, inplace=True)
-search_leads.reset_index(drop=True,inplace=True)
-#
-search_leads.to_csv(path_or_buf='twitter_output.csv', sep=',', encoding='utf-8')
+def prepare_search_query(config):
+    query = keywords['Keywords'][2] + " OR " + keywords['HashTags'][2]
+    if config['since']:
+        since = get_search_date(config['since'])
+        print(since)
+    if config['until']:
+        until = get_search_date(config['until'])
+        print(until)
+    if config['country'] or config['city']:
+        place = get_search_place(config)
+        query = "place:" + place + " " + query
+    # for key in keywords['Keywords'].append(keywords['HashTags']):
+    print(query)
+    search_twitter_for(query=query, since=since, until=until)
+
+
+prepare_search_query(config)
+
+# search_leads.drop_duplicates(subset=['Handle'], keep=False, inplace=True)
+# search_leads.reset_index(drop=True,inplace=True)
+# #
+# search_leads.to_csv(path_or_buf='twitter_output.csv', sep=',', encoding='utf-8')
